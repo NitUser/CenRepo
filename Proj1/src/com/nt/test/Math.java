09a130cd6c7e2d28902150bf00ee5af8ad29d68b
AKG <AKG@192.168.0.7>
2018-06-10 22:56:09 +0530

    add MAth method by Dev1

---
+++ Proj1/src/com/nt/test/Math.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+public class Math {
+
+	public static void main(String[] args) {
+	
+   math(10,20);
+	}
+ public static void math(int a, int b)
+ {
+	 System.out.println(a+b);
+ }
+}
