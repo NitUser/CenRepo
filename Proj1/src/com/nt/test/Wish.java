7bff95552751e9219cd2753a7e20d77ec5838066
AKG <AKG@192.168.0.7>
2018-06-10 22:41:55 +0530

    add 1 to 10 numver abcev1

--- Proj1/src/com/nt/test/Wish.java
+++ Proj1/src/com/nt/test/Wish.java
@@ -1,7 +1,12 @@
 package com.nt.test;
 public class Wish {
 public static void main(String[] args) {
-	 System.out.println("Hi.............Hello");
 	System.out.println("Good morning");
-	}
+    System.out.println("print number 1 to 10");
+	for(int i=1;i<=10;i++)
+  {
+	  System.out.println(i);
+  }
+}
+
 }
