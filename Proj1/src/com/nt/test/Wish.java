4daf466e86ea2a97da300ca4e9ae734467edd0d4
AKG <AKG@192.168.0.7>
2018-06-10 22:49:53 +0530

    update rajaTL

--- Proj1/src/com/nt/test/Wish.java
+++ Proj1/src/com/nt/test/Wish.java
@@ -1,7 +1,6 @@
 package com.nt.test;
 public class Wish {
 public static void main(String[] args) {
-	System.out.println("Good morning");
     System.out.println("print number 1 to 10");
 	for(int i=1;i<=10;i++)
   {
