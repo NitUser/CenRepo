f80663a44a5e729d4e8dbecedf77652b17fc209b
AKG <AKG@192.168.0.7>
2018-06-10 14:25:27 +0530

    RajaTL update

--- Proj1/src/com/nt/test/Wish.java
+++ Proj1/src/com/nt/test/Wish.java
@@ -1,7 +1,7 @@
 package com.nt.test;
 public class Wish {
 public static void main(String[] args) {
-	 System.out.println("Hi");
+	 System.out.println("Hi.............Hello");
 	System.out.println("Good morning");
 	}
 }
